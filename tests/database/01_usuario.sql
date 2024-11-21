63caa7b13bccd42b44ae674d0ab8975af62ca9c2
Rodrigotna <rodrigo15tna@gmail.com>
2024-11-21 15:03:08 -0300

    Update 01_usuario.sql

--- tests/database/01_usuario.sql
+++ tests/database/01_usuario.sql
@@ -1,1 +1,8 @@
--- TODO: Adicionar registros de teste para a tabela de usuario
+-- Tabela de usuario
+
+CREATE TABLE usuario (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nm_usuario VARCHAR(80) NOT NULL,
+    tp_posicao ENUM('aluno', 'professor', 'funcionario', 'visitante') NOT NULL,
+    ds_identificacao VARCHAR(50) NOT NULL
+);
