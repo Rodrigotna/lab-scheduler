717c47c45a9f2214c4fc0753278677a5b7b53395
Rodrigotna <rodrigo15tna@gmail.com>
2024-11-21 15:24:59 -0300

    Update 04_horario_laboratorio.sql

--- tests/database/04_horario_laboratorio.sql
+++ tests/database/04_horario_laboratorio.sql
@@ -1,1 +1,9 @@
--- TODO: Adicionar registros de teste para a tabela de associativa horario laboratório
+-- Tabela associativa laboratorio_horario
+CREATE TABLE laboratorio_horario (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    id_laboratorio INT NOT NULL,
+    id_horario INT NOT NULL,
+    is_reservado BOOLEAN DEFAULT FALSE,
+    FOREIGN KEY (id_laboratorio) REFERENCES laboratorio(id),
+    FOREIGN KEY (id_horario) REFERENCES horario(id)
+);
