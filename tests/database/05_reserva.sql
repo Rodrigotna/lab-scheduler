1986b125ddc4941d736398a31967b2af00dcb8a1
Rodrigotna <rodrigo15tna@gmail.com>
2024-11-21 15:25:35 -0300

    Update 05_reserva.sql

--- tests/database/05_reserva.sql
+++ tests/database/05_reserva.sql
@@ -1,1 +1,10 @@
--- TODO: Adicionar registros de teste para a tabela de reserva
+-- Tabela de reserva
+CREATE TABLE reserva (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    st_reserva ENUM('confirmada', 'finalizada', 'cancelada') NOT NULL,
+    tp_reserva VARCHAR(50) DEFAULT NULL,
+    id_usuario INT NOT NULL,
+    id_laboratorio_horario INT NOT NULL,
+    FOREIGN KEY (id_usuario) REFERENCES usuario(id),
+    FOREIGN KEY (id_laboratorio_horario) REFERENCES laboratorio_horario(id)
+);
