7778c3b0b6a64137c70c5a6c05466a46b1226ca8
Rodrigotna <rodrigo15tna@gmail.com>
2024-11-21 15:10:14 -0300

    Update 02_horario.sql

--- tests/database/02_horario.sql
+++ tests/database/02_horario.sql
@@ -1,1 +1,10 @@
--- TODO: Adicionar registros de teste para a tabela de horario
+-- Tabela de horario
+CREATE TABLE horario (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    dt_dia DATE NOT NULL,
+    ds_dia_semana ENUM('segunda', 'terça', 'quarta', 'quinta', 'sexta', 'sábado', 'domingo') NOT NULL,
+    hr_inicio TIME NOT NULL,
+    hr_fim TIME NOT NULL,
+    ds_semestre VARCHAR(10) NOT NULL,
+    nr_ano YEAR NOT NULL
+);
