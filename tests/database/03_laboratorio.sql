d68104aa480354870b49a0e6bc467dd02b00ec20
Rodrigotna <rodrigo15tna@gmail.com>
2024-11-21 15:24:08 -0300

    Update 03_laboratorio.sql

--- tests/database/03_laboratorio.sql
+++ tests/database/03_laboratorio.sql
@@ -1,1 +1,7 @@
--- TODO: Adicionar registros de teste para a tabela de laboratorio
+-- Tabela de laboratorio
+CREATE TABLE laboratorio (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    ds_bloco VARCHAR(10) NOT NULL,
+    ds_sala VARCHAR(10) NOT NULL,
+    qtd_pcs INT NOT NULL
+);
